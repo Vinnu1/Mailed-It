911e431efe77ac4f4bf7c7e2807d25b189775685
Vinayak Sinha <vinnusinha@gmail.com>
2017-04-23 20:30:09 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,129 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<link href="https://fonts.googleapis.com/css?family=Arvo:bold" rel="stylesheet">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<head>
+    <meta charset="UTF-8">
+    <title>Mailed It!</title>
+    <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
+<style>
+h1
+{
+font-family:'Arvo', serif;
+color:white;
+}
+
+body
+{
+font-family:'Arvo', serif;
+background-color:#C63D0F;
+text-align:center;
+}
+textarea
+{
+	resize:none;
+}
+.heading
+{
+width: 100%;
+padding-top: 20px;
+padding-bottom: 20px;
+text-align:center;
+background-color:#333333;
+}
+.foot
+{
+width: 100%;
+text-align:center; 
+background-color:white;
+padding-bottom:1px;
+bottom:0;
+position:fixed;
+}
+td{ 
+font-family:'Arvo', serif;
+    text-align:right;
+}
+table
+{ 
+top:0px;
+margin-left: auto;
+margin-right: auto;
+}
+.button 
+	{
+	background-color: #333333;
+    color: white;
+    border: 2px solid #333333;
+	-webkit-transition-duration: 0.4s; 
+    transition-duration: 0.4s;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    font-style:normal;
+	font-family:'Arvo', serif;
+	}
+.button:hover 
+	{
+	background-color: #FFFFFF;
+    color: black;
+	border: 2px solid #505050;
+    
+	}
+input[type=file]
+{
+	font-style:normal;
+}
+input
+{
+color: #333333;
+font-family: ‘Arial Narrow’, sans-serif;
+font-style:italic;
+}
+::-webkit-input-placeholder { /* WebKit, Blink, Edge */
+    font-family:'Arvo', serif;
+	font-style:normal;
+}
+textarea
+{
+font-family: ‘Arial Narrow’, sans-serif;
+font-style:italic;
+}
+a {
+	text-decoration:none;
+}
+</style>
+<body>
+<div class="heading">
+<h1> Mailed It! </a></h1>
+</div>
+<h3 style="color:white; text-align:center;">Send mails directly from here if you have Gmail, Yahoo or Outlook.<br>
+Note: For Gmail, first turn on 'access for less secure apps' in your account settings.</h3>
+<br>
+<br>
+    <form action="mailresult.php" method="post" enctype="multipart/form-data">
+		<table>
+		<tr><td>Email Service:</td><td style="text-align:left;"><select name="es">
+			<option value="gmail"> Gmail </option>
+			<option value="outlook"> Outlook </option>
+			<option value="yahoo"> Yahoo </option>
+			</select></td></tr>
+		<tr><td>Name:</td><td style="text-align:left;"><input type="text" name="n" placeholder="(Optional)"</td></tr>
+		<tr><td>Email:</td><td style="text-align:left;"><input type="email" name="e" ></td></tr>
+        <tr><td>Password:</td><td style="text-align:left;"><input type="password" name="p"></td></tr>
+        <tr><td>Receiver's Email:</td><td style="text-align:left;"><input type="email" name="re"></td></tr>
+		<tr><td>CC:</td><td style="text-align:left;"><input type="email" name="cc" placeholder="(Optional)"></td></tr>
+		<tr><td>BCC:</td><td style="text-align:left;"><input type="email" name="bcc" placeholder="(Optional)"></td></tr>
+		<tr><td>Subject:</td><td style="text-align:left;"><input type="text" name="sub"></td></tr>
+		<tr><td></td><td style="text-align:left;"><textarea rows=10 cols=40 name="body"></textarea></td></tr>
+		<tr><td>Attachment:</td><td style="text-align:left;"></label><input type="file" name="photo" id="fileSelect"></td></tr>
+        <tr><td></td><td style="text-align:left;"><br><br><input type="submit" name="submit" value="Send" class="button"></td></tr>
+		</table>
+	</form>
+</body>
+<br>
+<div class="foot" style="font-size:15px;">
+Contact Vinayak &nbsp <a style="color:#505050" href="https://www.linkedin.com/in/vinayak-sinha-743125110/" title="Linkedin" target=_blank> <img border="0" alt="Linkedin" src="linkedin.png" width="20px" height="20px"></a> &nbsp <a style="color:#505050" href="https://www.github.com/Vinnu1" title="GitHub" target=_blank> <img border="0" alt="GitHub" src="g.png" width="20px" height="20px"></a>
+</div>
+</html>
---
+++ mailresult.php
@@ -0,0 +1,164 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<link href="https://fonts.googleapis.com/css?family=Arvo:bold" rel="stylesheet">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<head>
+    <meta charset="UTF-8">
+    <title>Mailed It!</title>
+    <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
+<style>
+h1
+{
+font-family:'Arvo', serif;
+color:white;
+}
+
+body
+{
+font-family:'Arvo', serif;
+background-color:#C63D0F;
+text-align:center;
+font-size:20px;
+}
+textarea
+{
+	resize:none;
+}
+.heading
+{
+width: 100%;
+padding-top: 20px;
+padding-bottom: 20px;
+text-align:center;
+background-color:#333333;
+}
+td{ 
+font-family:'Arvo', serif;
+    text-align:right;
+}
+table
+{ 
+top:0px;
+margin-left: auto;
+margin-right: auto;
+}
+.button 
+	{
+	background-color: #333333;
+    color: white;
+    border: 2px solid #333333;
+	-webkit-transition-duration: 0.4s; 
+    transition-duration: 0.4s;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    font-style:normal;
+	font-family:'Arvo', serif;
+	}
+.button:hover 
+	{
+	background-color: #FFFFFF;
+    color: black;
+	border: 2px solid #505050;
+    
+	}
+input[type=file]
+{
+	font-style:normal;
+}
+input
+{
+color: #333333;
+font-family: ‘Arial Narrow’, sans-serif;
+font-style:italic;
+}
+::-webkit-input-placeholder { /* WebKit, Blink, Edge */
+    font-family:'Arvo', serif;
+	font-style:normal;
+}
+textarea
+{
+	font-family: ‘Arial Narrow’, sans-serif;
+font-style:italic;
+}
+a 
+{
+	text-decoration:none;
+}
+.foot
+{
+width: 100%;
+text-align:center; 
+background-color:white;
+position: absolute;
+bottom:0;
+padding-bottom:1px;
+}
+</style>
+<div class="heading">
+<h1> Mailed It! </h1>
+</div>
+<br>
+<br>
+<body>
+<?php
+if(isset($_POST['submit']))
+{
+require $_SERVER['DOCUMENT_ROOT'].'/vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
+
+$mail = new PHPMailer;
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();    
+switch($_POST['es'])
+{
+	case 'yahoo':                                   // Set mailer to use SMTP
+				$mail->Host = 'smtp.mail.yahoo.com';  // Specify main and backup SMTP servers
+				$mail->Port = 465;
+				$mail->SMTPSecure = 'ssl'; 
+				break;
+	case 'gmail':                                   // Set mailer to use SMTP
+				$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+				$mail->Port = 465;
+				$mail->SMTPSecure = 'ssl'; 
+				break;
+	case 'outlook':                                   // Set mailer to use SMTP
+				$mail->Host = 'smtp.live.com';  // Specify main and backup SMTP servers
+				$mail->Port = 587;
+				$mail->SMTPSecure = 'tls'; 
+				break;
+}
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = $_POST['e'];                 // SMTP username
+$mail->Password = $_POST['p'];                           // SMTP password
+$mail->setFrom($_POST['e'], $_POST['n']);
+$mail->addAddress($_POST['re'], '');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('vinnusinha@gmail.com', 'Vinnu');
+$mail->addCC($_POST['cc']);
+$mail->addBCC($_POST['bcc']);
+$mail->addAttachment($_FILES["photo"]["tmp_name"],$_FILES["photo"]["name"] );         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = $_POST['sub'];
+$mail->Body    = $_POST['body'];
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+	echo "<span style='color: black;'>"."Message could not be sent.<br>"."</span>";
+    echo "<span style='color: black;'>"."Error:"."Insert all required credentials correctly."."</span>";
+	
+} else {
+	echo "<span style='color:blue;'>"."Message has been sent."."</span>";
+	
+}
+}
+?>
+<div class="foot" style="font-size:15px;">
+Contact Vinayak &nbsp <a style="color:#505050" href="https://www.linkedin.com/in/vinayak-sinha-743125110/" title="Linkedin" target=_blank> <img border="0" alt="Linkedin" src="linkedin.png" width="20px" height="20px"></a> &nbsp <a style="color:#505050" href="https://www.github.com/Vinnu1" title="GitHub" target=_blank> <img border="0" alt="GitHub" src="g.png" width="20px" height="20px"></a>
+</div>
+</body>
+</form>
